01a54df017bc3ea6e51072f0cab899ae323529f9
ayush <you@example.com>
2018-01-10 22:41:10 +0530

    initial commit header section/ront page started

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public menuCtrl: MenuController) {
 
   }
+
+   openMenu() {
+   this.menuCtrl.open();
+	 }
+
+ closeMenu() {
+   this.menuCtrl.close();
+ 	}
+
+ toggleMenu() {
+   this.menuCtrl.toggle();
+ 	}
 
 }
